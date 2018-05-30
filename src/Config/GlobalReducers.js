c09fc012757961ad0b843152b64dd89a4267d817
Nik Mouzourides <nikolas.mouzourides@gmail.com>
2018-05-30 08:26:02 +0100

    Add redux and fix enemy reload on mouse move

--- src/Config/GlobalReducers.js
+++ src/Config/GlobalReducers.js
@@ -1,8 +1,8 @@
 import { combineReducers } from "redux";
 import { routerReducer } from "react-router-redux";
-import counter from "../Components/Canvas/CanvasActionReducer";
+import enemyReducer from "../Components/Enemy/EnemyActionReducer";
 
 export default combineReducers({
   router: routerReducer,
-  counter
+  enemyReducer
 });
