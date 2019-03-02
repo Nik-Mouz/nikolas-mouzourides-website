a225e6832be017dd15db409297825e3e45e4bf81
Nik Mouzourides <nikolas.mouzourides@gmail.com>
2019-03-02 18:35:54 +0000

    Init react

---
+++ src/Homepage/Homepage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Homepage.scss";
+
+const Homepage = () => {
+  return (
+    <div>
+      <h1>Hello world</h1>
+      <h2>By Nikolas Mouzourides</h2>
+    </div>
+  );
+};
+
+export default Homepage;
