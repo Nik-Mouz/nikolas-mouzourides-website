ac1d3fab24d8e7584742f355d43c1ec7e20636b4
Nik Mouzourides <nikolas.mouzourides@gmail.com>
2019-03-02 18:10:19 +0000

    Fresh start... again

--- src/Config/GlobalReducers.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import { routerReducer } from "react-router-redux";
-import enemyReducer from "../Components/Enemy/EnemyActionReducer";
-
-export default combineReducers({
-  router: routerReducer,
-  enemyReducer
-});
