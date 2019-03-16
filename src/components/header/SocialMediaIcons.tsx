c95a6f8f035c85050d40c07baa60a5b05fe1d0aa
Nik Mouzourides <nikolas.mouzourides@gmail.com>
2019-03-16 23:12:14 +0000

    Refactor icons into seperate component

---
+++ src/components/header/SocialMediaIcons.tsx
@@ -0,0 +1,27 @@
+import IconButton from "@material-ui/core/es/IconButton/IconButton";
+import * as React from "react";
+import TwitterIcon from "../../images/twitter-icon.svg";
+import LinkedinIcon from "../../images/linkedin-icon.svg";
+import GitHubIcon from "../../images/github-icon.png";
+import MailIcon from "../../images/email-icon.svg";
+
+const SocialMediaIcons = () => (
+  <>
+    <IconButton color="inherit" aria-label="Email" href="mailto:***REMOVED***">
+      <img src={MailIcon} alt="Mail icon"/>
+    </IconButton>
+    <IconButton color="inherit" aria-label="Linkedin"
+                href="https://www.github.com/mouzourides">
+      <img src={GitHubIcon} height="28" width="30" alt="GitHub icon"/>
+    </IconButton>
+    <IconButton color="inherit" aria-label="Twitter" href="https://twitter.com/Nik_Mouz">
+      <img src={TwitterIcon} alt="Twitter icon"/>
+    </IconButton>
+    <IconButton color="inherit" aria-label="Linkedin"
+                href="https://www.linkedin.com/in/nikolas-mouzourides-894b45113/">
+      <img src={LinkedinIcon} alt="Linkedin icon"/>
+    </IconButton>
+  </>
+);
+
+export default SocialMediaIcons;
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,10 +1,6 @@
 import React, { useState } from "react";
 import Typography from "@material-ui/core/Typography";
 import IconButton from "@material-ui/core/IconButton";
-import TwitterIcon from "../../images/twitter-icon.svg";
-import LinkedinIcon from "../../images/linkedin-icon.svg";
-import GitHubIcon from "../../images/github-icon.png";
-import MailIcon from "../../images/email-icon.svg";
 import MenuIcon from "../../images/menu-icon.svg";
 import { Link } from "gatsby";
 import "./header.scss";
@@ -13,6 +9,7 @@ import ListItem from "@material-ui/core/es/ListItem/ListItem";
 import ListItemText from "@material-ui/core/es/ListItemText/ListItemText";
 import Divider from "@material-ui/core/es/Divider/Divider";
 import SwipeableDrawer from "@material-ui/core/es/SwipeableDrawer/SwipeableDrawer";
+import SocialMediaIcons from "./SocialMediaIcons";
 
 interface HeaderProps {
   title: string;
@@ -33,20 +30,7 @@ const Header = (props: HeaderProps) => {
       </div>
       <div className="d-flex justify-content-around">
         <div className="nav-icons no-display-mobile">
-          <IconButton color="inherit" aria-label="Email" href="mailto:***REMOVED***">
-            <img src={MailIcon} alt="Mail icon"/>
-          </IconButton>
-          <IconButton color="inherit" aria-label="Linkedin"
-                      href="https://www.github.com/mouzourides">
-            <img src={GitHubIcon} height="28" width="30" alt="GitHub icon"/>
-          </IconButton>
-          <IconButton color="inherit" aria-label="Twitter" href="https://twitter.com/Nik_Mouz">
-            <img src={TwitterIcon} alt="Twitter icon"/>
-          </IconButton>
-          <IconButton color="inherit" aria-label="Linkedin"
-                      href="https://www.linkedin.com/in/nikolas-mouzourides-894b45113/">
-            <img src={LinkedinIcon} alt="Linkedin icon"/>
-          </IconButton>
+          <SocialMediaIcons/>
         </div>
         <IconButton className="menu-icon" color="inherit" aria-label="Menu" onClick={() => {
           setOpen(!open);
