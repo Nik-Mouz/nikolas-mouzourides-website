59283115a61f82defd3dd2d103527f0915a4876a
Nik Mouzourides <nikolas.mouzourides@gmail.com>
2019-03-16 23:12:31 +0000

    Add icons to footer and footer style changes

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,11 +1,15 @@
 import * as React from "react";
 import "./footer.scss"
+import SocialMediaIcons from "../header/SocialMediaIcons";
 
 const Footer = () => {
   return (
     <footer className="footer w-100 p-3">
-      <div className="container">
+      <div className="container text-center d-flex flex-column">
         <span>Nikolas Mouzourides © {new Date().getFullYear()}</span>
+        <div>
+          <SocialMediaIcons/>
+        </div>
       </div>
     </footer>
   )
