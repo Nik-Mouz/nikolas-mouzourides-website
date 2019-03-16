b3ad162dfcd9848d6e2925bf45f8c1eed784ec7d
Nik Mouzourides <nikolas.mouzourides@gmail.com>
2019-03-16 17:31:19 +0000

    Styling update to footer and layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "gatsby";
 
 import Layout from "../components/layout/layout";
 import Seo from "../components/seo/seo";
@@ -8,9 +7,6 @@ const IndexPage = () => (
   <Layout>
     <Seo title="Home" description="Nikolas Mouzourides home page" keywords={[`gatsby`, `application`, `react`]}/>
     <h1>Hello world</h1>
-    {/* <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}> */}
-    {/* <Image /> */}
-    {/* </div> */}
   </Layout>
 );
 
