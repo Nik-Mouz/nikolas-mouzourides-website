0035948415910949b93c76c7073fbb01bdb0b950
Nik Mouzourides <nikolas.mouzourides@gmail.com>
2019-03-17 01:31:32 +0000

    Work on blog cards and blog page

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,13 +1,21 @@
 import React from "react";
-
+import BlogHeroImage from "../images/blog-hero.jpg";
 import Layout from "../components/layout/layout";
 import Seo from "../components/seo/seo";
+import BlogCard from "../components/card/card";
 
 const SecondPage = () => (
-  <Layout>
+  <Layout heroImage={<img className="hero" style={{height: 400}} src={BlogHeroImage}/>}>
     <Seo title="Blog" description="Nik Mouzourides blog"/>
-    <h1>Blog</h1>
-    <p>Welcome to the blog</p>
+    <h1 className="pb-2">Blog</h1>
+    <div className="d-flex flex-row flex-wrap">
+      <BlogCard title="Blog" description="This is a blog post about blog posts" imagePath={BlogHeroImage}/>
+      <BlogCard title="Blog" description="This is a blog post about blog posts" imagePath={BlogHeroImage}/>
+      <BlogCard title="Blog" description="This is a blog post about blog posts" imagePath={BlogHeroImage}/>
+      <BlogCard title="Blog" description="This is a blog post about blog posts" imagePath={BlogHeroImage}/>
+      <BlogCard title="Blog" description="This is a blog post about blog posts" imagePath={BlogHeroImage}/>
+      <BlogCard title="Blog" description="This is a blog post about blog posts" imagePath={BlogHeroImage}/>
+    </div>
   </Layout>
 );
 
