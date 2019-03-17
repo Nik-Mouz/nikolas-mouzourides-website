fa2c7260311da4a9b5029bc9295eaf6719f1328a
Nik Mouzourides <nikolas.mouzourides@gmail.com>
2019-03-17 01:31:32 +0000

    Work on blog cards and blog page

---
+++ src/components/card/card.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import CardMedia from "@material-ui/core/es/CardMedia/CardMedia";
+import CardActionArea from "@material-ui/core/es/CardActionArea/CardActionArea";
+import CardContent from "@material-ui/core/es/CardContent/CardContent";
+import Typography from "@material-ui/core/es/Typography/Typography";
+import Card from "@material-ui/core/es/Card/Card";
+import "./card.scss";
+
+interface CardProps {
+  title: string;
+  description: string;
+  imagePath: string;
+}
+
+const BlogCard = (props: CardProps) => (
+  <>
+    <Card className="card mb-4">
+      <CardActionArea>
+        <CardMedia title="Card"/>
+        <img className="img-fluid" height="200px" src={props.imagePath}/>
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {props.title}
+          </Typography>
+          <Typography component="p">
+            {props.description}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  </>
+);
+
+export default BlogCard;
