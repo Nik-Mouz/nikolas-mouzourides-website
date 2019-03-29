f7e4987ff72176741c313349ad28951abf004070
Nik Mouzourides <nikolas.mouzourides@gmail.com>
2019-03-29 23:40:34 +0000

    Make links to external websites open in new tab

--- src/components/social-media-icons/social-media-icons.tsx
+++ src/components/social-media-icons/social-media-icons.tsx
@@ -11,14 +11,14 @@ const SocialMediaIcons = () => (
       <img src={MailIcon} alt="Mail icon"/>
     </IconButton>
     <IconButton color="inherit" aria-label="Linkedin"
-                href="https://www.github.com/mouzourides">
+                href="https://www.github.com/mouzourides" target="_blank">
       <img src={GitHubIcon} height="28" width="30" alt="GitHub icon"/>
     </IconButton>
-    <IconButton color="inherit" aria-label="Twitter" href="https://twitter.com/Nik_Mouz">
+    <IconButton color="inherit" aria-label="Twitter" href="https://twitter.com/Nik_Mouz" target="_blank">
       <img src={TwitterIcon} alt="Twitter icon"/>
     </IconButton>
     <IconButton color="inherit" aria-label="Linkedin"
-                href="https://www.linkedin.com/in/nikolas-mouzourides-894b45113/">
+                href="https://www.linkedin.com/in/nikolas-mouzourides-894b45113/" target="_blank">
       <img src={LinkedinIcon} alt="Linkedin icon"/>
     </IconButton>
   </>
