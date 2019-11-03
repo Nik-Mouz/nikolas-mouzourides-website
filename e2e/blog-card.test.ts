09b9b7707afd2ad8ef496cae636af4d67dad5b40
Nik Mouzourides <nikolas.mouzourides@gmail.com>
2019-11-03 22:07:31 +0000

    Fix test

--- e2e/blog-card.test.ts
+++ e2e/blog-card.test.ts
@@ -4,11 +4,11 @@ fixture`Blog cards`
   .page`http://localhost:8000/`;
 
 test("Blog cards should be less than or equal to 3 on the home page", async t => {
-  await t.expect(Selector("[data-acctest='blog-card']").count).lte(3);
+  await t.expect(Selector("[data-acctest='blog-card']").count).eql(3);
 });
 
 test("Blog cards should be equal to the amount of blogs written", async t => {
   await t
     .navigateTo("/blog")
-    .expect(Selector("[data-acctest='blog-card']").count).lte(3);
+    .expect(Selector("[data-acctest='blog-card']").count).gt(3);
 });
