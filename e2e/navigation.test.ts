47366b909c0b3dded348ec29acafe710199316fd
Nik Mouzourides <nikolas.mouzourides@gmail.com>
2019-11-15 22:13:14 +0000

    Move resize to before step in mobile nav test

--- e2e/navigation.test.ts
+++ e2e/navigation.test.ts
@@ -7,13 +7,12 @@ test("The navigation bar should display desktop components when on desktop viewp
   await t.expect(Selector(".no-display-mobile").visible).ok();
 });
 
-test("The navigation bar should display mobile components when on mobile viewport", async t => {
-  await t
-    .resizeWindow(375, 667)
-    .wait(500)
-    .expect(Selector("[data-acctest='mobile-menu']").visible).ok()
-    .click(Selector("[data-acctest='mobile-menu']"))
-    .expect(Selector("[data-acctest='mobile-drawer']").visible).ok()
-    .click(Selector("[data-acctest='drawer-close']"))
-    .expect(Selector("[data-acctest='mobile-drawer']").exists).notOk();
-});
+test.before(t => t.resizeWindow(375, 667))
+  ("The navigation bar should display mobile components when on mobile viewport", async t => {
+    await t
+      .expect(Selector("[data-acctest='mobile-menu']").visible).ok()
+      .click(Selector("[data-acctest='mobile-menu']"))
+      .expect(Selector("[data-acctest='mobile-drawer']").visible).ok()
+      .click(Selector("[data-acctest='drawer-close']"))
+      .expect(Selector("[data-acctest='mobile-drawer']").exists).notOk();
+  });
